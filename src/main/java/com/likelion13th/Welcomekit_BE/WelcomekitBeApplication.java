f59ab41987c37723b8ccc3ace34dab6922c67fc3
홍길동 <@naver.com>
2025-03-10 16:12:32 +0900

    setting

---
+++ src/main/java/com/likelion13th/Welcomekit_BE/WelcomekitBeApplication.java
@@ -0,0 +1,13 @@
+package com.likelion13th.Welcomekit_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WelcomekitBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WelcomekitBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/likelion13th/Welcomekit_BE/WelcomekitBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likelion13th.Welcomekit_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WelcomekitBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
