91ad6afa71a5fb4198d14fbc64761fe0e672c110
HyunWoo9930 <ohw9930@hufs.ac.kr>
2025-03-13 23:45:03 +0900

    [FEAT] : swagger 서버 추가

--- src/main/java/com/likelion13th/Welcomekit_BE/config/SwaggerConfig.java
+++ src/main/java/com/likelion13th/Welcomekit_BE/config/SwaggerConfig.java
@@ -1,5 +1,7 @@
 package com.likelion13th.Welcomekit_BE.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,6 +10,7 @@ import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
 public class SwaggerConfig {
@@ -22,8 +25,15 @@ public class SwaggerConfig {
 				.name("HyunWoo9930")
 				.email("hw62459930@gmail.com"));
 
+		String localServer = "http://localhost:8080";
+		String productionServer = "https://welcomekitbe.lion.it.kr";
+
 		return new OpenAPI()
 			.info(info)
+			.servers(List.of(
+				new Server().url(localServer).description("Local Server"),
+				new Server().url(productionServer).description("Production Server")
+			))
 			.addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
 			.components(new io.swagger.v3.oas.models.Components()
 				.addSecuritySchemes("Bearer Authentication", new SecurityScheme()
