e1b12739f20af312a0df74e247180b105756632e
HyunWoo9930 <ohw9930@hufs.ac.kr>
2025-03-15 05:32:43 +0900

    [FIX] : CORS 에러 해결

--- src/main/java/com/likelion13th/Welcomekit_BE/controller/TeamController.java
+++ src/main/java/com/likelion13th/Welcomekit_BE/controller/TeamController.java
@@ -2,6 +2,7 @@ package com.likelion13th.Welcomekit_BE.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,7 @@ import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*")
 @RequestMapping("/api/team")
 public class TeamController {
 	@Autowired
--- src/main/java/com/likelion13th/Welcomekit_BE/controller/UserController.java
+++ src/main/java/com/likelion13th/Welcomekit_BE/controller/UserController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,6 +18,7 @@ import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*")
 @RequestMapping("/api/user")
 public class UserController {
 
