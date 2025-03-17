e3f68bc2ece3ad73cbd11fe8f6284da5d98fd251
HyunWoo9930 <ohw9930@hufs.ac.kr>
2025-03-17 17:18:41 +0900

    Feat : mission 증가

--- src/main/java/com/likelion13th/Welcomekit_BE/domain/enums/BingoEnum.java
+++ src/main/java/com/likelion13th/Welcomekit_BE/domain/enums/BingoEnum.java
@@ -15,7 +15,22 @@ public enum BingoEnum {
 	MISSION_7("일곱 번째 미션"),
 	MISSION_8("여덟 번째 미션"),
 	MISSION_9("아홉 번째 미션"),
-	MISSION_10("열 번째 미션");
+	MISSION_10("열 번째 미션"),
+	MISSION_11("열 번째 미션"),
+	MISSION_12("열 번째 미션"),
+	MISSION_13("열 번째 미션"),
+	MISSION_14("열 번째 미션"),
+	MISSION_15("열 번째 미션"),
+	MISSION_16("열 번째 미션"),
+	MISSION_17("열 번째 미션"),
+	MISSION_18("열 번째 미션"),
+	MISSION_19("열 번째 미션"),
+	MISSION_20("열 번째 미션"),
+	MISSION_21("열 번째 미션"),
+	MISSION_22("열 번째 미션"),
+	MISSION_23("열 번째 미션"),
+	MISSION_24("열 번째 미션"),
+	MISSION_25("열 번째 미션");
 
 	private final String description;
 
