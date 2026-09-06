f77824998d783083c8c4b51a89e8ab8c9b287f14
juhui <juhui4078@hufs.ac.kr>
2026-09-06 18:11:51 +0900

    feat: BingoManager에 verifyCell 위임 메서드 추가

--- src/main/java/com/likelion13th/Welcomekit_BE/manager/BingoManager.java
+++ src/main/java/com/likelion13th/Welcomekit_BE/manager/BingoManager.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import com.likelion13th.Welcomekit_BE.domain.User;
 import com.likelion13th.Welcomekit_BE.domain.dto.response.BingoBoardResponse;
+import com.likelion13th.Welcomekit_BE.domain.dto.response.BingoVerifyResponse;
 import com.likelion13th.Welcomekit_BE.service.BingoService;
 import com.likelion13th.Welcomekit_BE.service.UserService;
 
@@ -21,5 +22,10 @@ public class BingoManager {
 	public BingoBoardResponse getBingoBoard(String email) {
 		User user = userService.getUserByEmail(email);
 		return bingoService.getBingoBoard(user);
+	}
+
+	public BingoVerifyResponse verifyCell(String email, Integer cellId, String opponentCode) {
+		User user = userService.getUserByEmail(email);
+		return bingoService.verifyCell(user, cellId, opponentCode);
 	}
 }
