a7eb3182a59767af1e8b76d6f72046cf444ba2ba
juhui <juhui4078@hufs.ac.kr>
2026-09-06 17:05:28 +0900

    refactor: BingoRepository 조회 기준을 Team에서 User로 변경, 코드 중복 확인용 existsByCode 추가

--- src/main/java/com/likelion13th/Welcomekit_BE/repository/BingoRepository.java
+++ src/main/java/com/likelion13th/Welcomekit_BE/repository/BingoRepository.java
@@ -5,8 +5,10 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.likelion13th.Welcomekit_BE.domain.Bingo;
-import com.likelion13th.Welcomekit_BE.domain.Team;
+import com.likelion13th.Welcomekit_BE.domain.User;
 
 public interface BingoRepository extends JpaRepository<Bingo, Long> {
-	Optional<Bingo> findByTeam(Team team);
+	Optional<Bingo> findByUser(User user);
+
+	boolean existsByCode(String code);
 }
