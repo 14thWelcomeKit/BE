2c6672376286be4ac197d149c57f550122d1351c
juhui <juhui4078@hufs.ac.kr>
2026-09-06 17:06:35 +0900

    feat: 빙고 도메인 전용 예외(BingoException) 추가

---
+++ src/main/java/com/likelion13th/Welcomekit_BE/exception/BingoException.java
@@ -0,0 +1,17 @@
+package com.likelion13th.Welcomekit_BE.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class BingoException extends RuntimeException {
+
+	private final HttpStatus httpStatus;
+	private final String code;
+
+	public BingoException(HttpStatus httpStatus, String code, String message) {
+		super(message);
+		this.httpStatus = httpStatus;
+		this.code = code;
+	}
+}
