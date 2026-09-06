14d3e21598491beb755c3a59bd439780ad216a68
juhui <juhui4078@hufs.ac.kr>
2026-09-06 17:06:24 +0900

    refactor: BingoManager를 빙고판 조회 흐름에 맞게 정리

--- src/main/java/com/likelion13th/Welcomekit_BE/manager/BingoManager.java
+++ src/main/java/com/likelion13th/Welcomekit_BE/manager/BingoManager.java
@@ -1,16 +1,11 @@
 package com.likelion13th.Welcomekit_BE.manager;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
-import com.likelion13th.Welcomekit_BE.domain.Team;
 import com.likelion13th.Welcomekit_BE.domain.User;
-import com.likelion13th.Welcomekit_BE.domain.dto.response.GetMyBingoResponse;
+import com.likelion13th.Welcomekit_BE.domain.dto.response.BingoBoardResponse;
 import com.likelion13th.Welcomekit_BE.service.BingoService;
-import com.likelion13th.Welcomekit_BE.service.TeamService;
 import com.likelion13th.Welcomekit_BE.service.UserService;
 
 import lombok.RequiredArgsConstructor;
@@ -22,22 +17,9 @@ public class BingoManager {
 	private final BingoService bingoService;
 	@Autowired
 	private final UserService userService;
-	@Autowired
-	private final TeamService teamService;
 
-	public List<GetMyBingoResponse> getMyBingo(String email) {
+	public BingoBoardResponse getBingoBoard(String email) {
 		User user = userService.getUserByEmail(email);
-		return bingoService.getMyBingo(user);
-	}
-
-	public String revealBingoCell(String email, Long id) {
-		User user = userService.getUserByEmail(email);
-		return bingoService.revealBingoCell(user, id);
-	}
-
-	public void approveTeam(UserDetails userDetails, String teamName) {
-		Team team = teamService.getTeam(teamName);
-		User user = userService.getUserByEmail(userDetails.getUsername());
-		bingoService.approveTeam(user, team);
+		return bingoService.getBingoBoard(user);
 	}
 }
